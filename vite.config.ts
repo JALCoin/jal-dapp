0a9b68449d93e7ffb4130f1a304a3ff3a4b518e2
JALCoin <jezzal@outlook.com>
2025-07-23 18:45:12 +0800

    Fix: revert polyfill config for working Step 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 // vite.config.ts
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import path from 'path';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
   resolve: {
     alias: {
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
-      sys: 'util',
-      events: 'rollup-plugin-node-polyfills/polyfills/events',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      path: 'rollup-plugin-node-polyfills/polyfills/path',
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+      buffer: path.resolve(__dirname, 'node_modules/buffer/'),
+      process: path.resolve(__dirname, 'node_modules/process/browser.js'),
     },
   },
   optimizeDeps: {
@@ -24,11 +21,12 @@ export default defineConfig({
       define: {
         global: 'globalThis',
       },
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
     },
   },
 });
