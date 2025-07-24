c4fa8b35a080434a0e1f04143900cc136df4b3a1
JALCoin <jezzal@outlook.com>
2025-07-24 21:14:34 +0800

    Fix: updated polyfills and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Polyfills
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
-      path: 'rollup-plugin-node-polyfills/polyfills/path',
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+      stream: 'stream-browserify',
+      crypto: 'crypto-browserify',
+      path: 'path-browserify',
+      buffer: 'buffer',
+      process: 'process/browser',
     },
   },
   optimizeDeps: {
@@ -20,7 +18,6 @@ export default defineConfig({
       define: {
         global: 'globalThis',
       },
-      inject: [require.resolve('rollup-plugin-node-polyfills/polyfills/global')],
     },
   },
   build: {
