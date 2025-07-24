f835114fe709a2081f03b83b2939556b6d195112
JALCoin <jezzal@outlook.com>
2025-07-24 21:18:12 +0800

    Fix: corrected process polyfill path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       crypto: 'crypto-browserify',
       path: 'path-browserify',
       buffer: 'buffer/',
-      process: 'process/browser.js', // ✅ fix here!
+      process: 'process/browser', // ✅ <-- fixed here
     },
   },
   optimizeDeps: {
