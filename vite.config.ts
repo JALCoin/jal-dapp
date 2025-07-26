33d0a001c9e426742972ee9f8c30faaad5ab5314
JALCoin <jezzal@outlook.com>
2025-07-26 21:40:33 +0800

    Fix: import defineConfig and plugins for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
+
 export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
   },
+  optimizeDeps: {
+    include: ['buffer', 'process'],
+  },
   build: {
-    sourcemap: true, // <== add this
+    sourcemap: true,
     rollupOptions: {
       plugins: [rollupNodePolyFill()],
     },
