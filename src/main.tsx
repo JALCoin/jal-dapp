8c2aaa7f06c7ed092875d5a021a617714ec35113
JALCoin <jezzal@outlook.com>
2025-07-26 21:58:11 +0800

    Fix: synchronous polyfill setup for Buffer and process

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,14 @@
+import { Buffer } from 'buffer';
+import process from 'process';
+
+(window as any).Buffer = Buffer;
+(window as any).process = process;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProviders } from './AppProviders';
 import './index.css';
-
-// Polyfills for Buffer and process in browser
-if (!(window as any).Buffer) {
-  import('buffer').then(({ Buffer }) => {
-    (window as any).Buffer = Buffer;
-  });
-}
-
-if (!(window as any).process) {
-  import('process').then((p) => {
-    (window as any).process = p.default;
-  });
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
