b270ab5aa7ba3283f28d93afbf6d93417153a222
JALCoin <jezzal@outlook.com>
2025-07-27 10:21:18 +0800

    Fix: safely guard TokenFinalizerModal render

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import { Buffer } from 'buffer';
-import process from 'process';
-
-(window as any).Buffer = Buffer;
-(window as any).process = process;
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppProviders } from './AppProviders';
 import './index.css';
+
+// Polyfills
+import { Buffer } from 'buffer';
+import process from 'process';
+(window as any).Buffer = Buffer;
+(window as any).process = process;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
+    <App />
   </React.StrictMode>
 );
